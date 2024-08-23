39ad05d770cde7de7e799fa2b3164a645b66d164
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-08-22 19:02:37 -0700

    adding/removing dbg from main body to prevent glitch

--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import { SwitchTransition, CSSTransition } from "react-transition-group";
@@ -7,12 +7,22 @@ import useTheme from '../../hooks/useTheme';
 function Layout() {
   const location = useLocation();
   let {isDark} = useTheme()
+
+  useEffect(()=>{
+    let body = document.body;
+    if(isDark){
+      body.classList.add('bg-dbg')
+    } else {
+      body.classList.remove('bg-dbg')
+    }
+  }, [isDark])
   return (
     <div className={isDark ? 'bg-dbg' : 'bg-white'}>
         <Navbar/>
 
         <SwitchTransition>
             <CSSTransition timeout={200} classNames='fade' key={location.pathname}> 
+                
                 <div className='max-w-6xl mx-auto p-3'>
                     <Outlet/>
                 </div> 
